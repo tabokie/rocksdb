36f2a1e8fcae602d41529c21e540c7c5686ab420
tabokie <xy.tao@outlook.com>
2020-08-11 11:51:20 +0800

    fix rate limiter
    Signed-off-by: tabokie <xy.tao@outlook.com>

--- util/file_reader_writer.cc
+++ util/file_reader_writer.cc
@@ -397,30 +397,8 @@ Status WritableFileWriter::Flush() {
   //     the page.
   // Xfs does neighbor page flushing outside of the specified ranges. We
   // need to make sure sync range is far from the write offset.
-  if (!use_direct_io() && rate_limiter_ != nullptr) {
-    const uint64_t kBytesNotSyncRange = 1024 * 1024;  // recent 1MB is not synced.
-    const uint64_t kBytesAlignWhenSync = 4 * 1024;    // Align 4KB.
-    if (filesize_ > kBytesNotSyncRange) {
-      uint64_t offset_sync_to = filesize_ - kBytesNotSyncRange;
-      offset_sync_to -= offset_sync_to % kBytesAlignWhenSync;
-      assert(offset_sync_to >= last_sync_size_);
-      if (offset_sync_to > 0) {
-        uint64_t pos = last_sync_size_;
-        uint64_t allowed = 0;
-        while (offset_sync_to > pos) {
-          allowed = rate_limiter_->RequestToken(
-              offset_sync_to - pos, 0, writable_file_->GetIOPriority(), stats_,
-              RateLimiter::OpType::kWrite);
-          s = RangeSync(pos, allowed);
-          if (s.ok()) {
-            last_sync_size_ = pos;
-            pos += allowed;
-          } else {
-            break;
-          }
-        }
-      }
-    }
+  if (s.ok() && !use_direct_io()) {
+    s = IncrementalSync();
   }
 
   return s;
@@ -490,8 +468,24 @@ Status WritableFileWriter::IncrementalSync() {
     assert(offset_sync_to >= last_sync_size_);
     if (offset_sync_to > 0 &&
         offset_sync_to - last_sync_size_ >= bytes_per_sync_) {
-      s = RangeSync(last_sync_size_, offset_sync_to - last_sync_size_);
-      last_sync_size_ = offset_sync_to;
+      uint64_t pos = last_sync_size_;
+      uint64_t allowed = 0;
+      while (offset_sync_to - pos >= bytes_per_sync_) {
+        if (rate_limiter_) {
+          allowed = rate_limiter_->RequestToken(
+              offset_sync_to - pos, 0, writable_file_->GetIOPriority(), stats_,
+              RateLimiter::OpType::kWrite);
+        } else {
+          allowed = offset_sync_to - pos;
+        }
+        s = RangeSync(pos, allowed);
+        if (s.ok()) {
+          last_sync_size_ = pos;
+          pos += allowed;
+        } else {
+          break;
+        }
+      }
     }
   }
   return s;
